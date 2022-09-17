b5e725acd96020273838246d31affa98277ae5b8
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-17 17:59:56 +0530

    performed changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <div class="other-header-container">
-        <div class="other-header-main">
-            <div class="other-header-logo">
-                {/* <img src={require('./images/Sandstone logo.png')}  width='290px'/> */}
-            </div>
-            <div class="other-header-list">
-                <ul>
-                    <Link to={'/'} 
-                    className='other-header-list-link'>Home</Link>
-                    <Link to={'/speakers'} className='other-header-list-link'>Speakers</Link>
-                    <Link to={'/sponsors'} className='other-header-list-link'>Sponsors</Link>
-                    <Link to={'/team'} className='other-header-list-link'>Core Team</Link>
-                    <Link to={'/hackathon'} className='other-header-list-link'>Events</Link>
-                    <Link to={'/theme'} className='other-header-list-link'>Theme</Link>
-                    {/* <li>Speakers</li>
+            <div class="other-header-main">
+                <div class="other-header-logo">
+                    {/* <img src={require('./images/Sandstone logo.png')}  width='290px'/> */}
+                </div>
+                <div class="other-header-list">
+                    <ul>
+                        <Link to={'/'}
+                            className='other-header-list-link'>Home</Link>
+                        <Link to={'/speakers'} className='other-header-list-link'>Speakers</Link>
+                        <Link to={'/sponsors'} className='other-header-list-link'>Sponsors</Link>
+                        <Link to={'/team'} className='other-header-list-link'>Core Team</Link>
+                        <Link to={'/hackathon'} className='other-header-list-link'>Events</Link>
+                        <Link to={'/theme'} className='other-header-list-link'>Theme</Link>
+                        {/* <li>Speakers</li>
                     <li>Sponsors</li>
                     <li>Core Team</li>
                     <li>Hackathon</li> */}
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
+            </div>
+            <div class="header-background-image">
+                {/* <img src={require('./images/Header/background image.png')} /> */}
+                {
+                    /* Add gradient as set in css */
+                }
+            </div>
         </div>
-        <div class="header-background-image">
-            {/* <img src={require('./images/Header/background image.png')} /> */}
-            {
-                /* Add gradient as set in css */
-            }
-        </div>
-    </div>
     )
 }
