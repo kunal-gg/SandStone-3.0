42794d6e189d545ab2ab2f302874a14bf54e22b3
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-17 18:36:02 +0530

    updated homepage.js

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -10,7 +10,7 @@ export default function Introduction() {
                 <div className="introduction-left-section">
                     <AnimationOnScroll animateIn="animate__slideInLeft ">
 
-                    <h1>India Accelerates:<br /> <span> Canvassing Vicennial</span> Sustainability</h1>
+                    <h1>India Accelerates:<br />  Canvassing Vicennial Sustainability</h1>
                     <p>
                         Sandstone signifies cohesiveness- a strong and evolving structure symbolizing growth and improvement. As the world is slowly freeing itself from the forceful clutches of the pandemic and clambering on stable grounds, we cannot ignore the rapid magnifications of every existing inequality caused by our recent plight which did not spare the corporate world. Staying true to its name, the SandStone summit boasts of four dynamic verticals, Impressario, HR Shastra, Praesidium, and the Eleutheria series and with the main aim of Evolving into the new age of Business. The summit ensures substantial interaction of the young minds with the figureheads of the corporate world and serves as a reminder that small changes can mold and shape them into future business leaders.
                     </p>
