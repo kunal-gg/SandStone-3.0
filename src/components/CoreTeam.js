5f509487c8bc5fbb93fb7c634eeaa41928de15fe
Pranavchiku <goswami.4@iitj.ac.in>
2022-09-19 00:34:47 +0530

    Updated core team

--- src/components/CoreTeam.js
+++ src/components/CoreTeam.js
@@ -3,129 +3,142 @@ import Card from "./CoreTeamCards";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
-export default function CoreTeam(){
-    return(
-        <div className="core-team-main">
-            <div className="core-team-heading">
-                <h2>Our <span>Team</span></h2>
-                <p>The team that has put their heart and soul into this event</p>
-            </div>
-           
+export default function CoreTeam() {
+  return (
+    <div className="core-team-main">
+      <div className="core-team-heading">
+        <h2>Our <span>Team</span></h2>
+        <p>The team that has put their heart and soul into this event</p>
+      </div>
 
-<AnimationOnScroll animateIn="animate__slideInLeft" animateOnce='true'>
-<div className="core-team-grid">
 
-<Card 
-    image={require('./images/Anuj Sir.jpeg')}
-    name='Anuj Pal Kapoor'
-    POR='CHAIRPERSON'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+      <AnimationOnScroll animateIn="animate__slideInLeft" animateOnce='true'>
+        <div className="core-team-grid">
 
-    <Card 
-    image={require('./images/Neeraj Pachar.png')}
-    name='Neeraj Pachar'
-    POR='SENIOR MANAGER'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+          <Card
+            image={require('./images/Anuj Sir.jpeg')}
+            name='Anuj Pal Kapoor'
+            POR='CHAIRPERSON'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    <Card 
-    image={require('./images/Aman Prakash.png')}
-    name='Aman Prakash'
-    POR='GENERAL SECRETARY-SS'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+          <Card
+            image={require('./images/Neeraj Pachar.png')}
+            name='Neeraj Pachar'
+            POR='SENIOR MANAGER'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    <Card 
-    image={require('./images/Manav Kapoor.jpg')}
-    name='Manav Kapoor'
-    POR='GENERAL SECRETARY - ACAC'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+          <Card
+            image={require('./images/Aman Prakash.png')}
+            name='Aman Prakash'
+            POR='GENERAL SECRETARY-SS'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    <Card 
-    image={require('./images/Jeevesh Jain.png')}
-    name='Jeevesh Jain'
-    POR='VICE PRESIDENT OF BCD'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+          <Card
+            image={require('./images/Puneet Garg.jpeg')}
+            name='Puneet Garg'
+            POR='CHAIRPERSON'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    <Card 
-    image={require('./images/Paridhi Mathur.png')}
-    name='Paridhi Mathur'
-    POR='SECRETARY'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
+          <Card
+            image={require('./images/Gurpreet Kaur Virdi.jpeg')}
+            name='Gurpreet Kaur Virdi'
+            POR='CHAIRPERSON'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
+          <Card
+            image={require('./images/Manav Kapoor.jpg')}
+            name='Manav Kapoor'
+            POR='GENERAL SECRETARY - ACAC'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    
+          <Card
+            image={require('./images/Jeevesh Jain.png')}
+            name='Jeevesh Jain'
+            POR='VICE PRESIDENT OF BCD'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
-    <Card 
-    image={require('./images/Aviral Aggarwal.png')}
-    name= 'Aviral Aggarwal'
-    POR='JOINT SECRETARY'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
-    <Card 
-    image={require('./images/Devanshi Grover.png')}
-    name='Devanshi Grover'
-    POR='JOINT SECRETARY'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-
-    />
-
-   
-
-    <Card 
-      image={require('./images/Madhumay Bhartiya.png')}
-      name='Madhumay Bhartiya'
-      POR='JOINT SECRETARY'
-      description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum'
-    />
+          <Card
+            image={require('./images/Paridhi Mathur.png')}
+            name='Paridhi Mathur'
+            POR='SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
 
 
-    <Card 
-    image={require('./images/Mayank Sharma.png')}
-    name='Mayank Sharma'
-    POR='JOINT SECRETARY'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
-  
-    <Card 
-    image={require('./images/Prodorshika Ghosh.png')}
-    name='Prodorshika Ghosh'
-    POR='JOINT SECRETARY'
-    description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-    />
-    <Card 
-    
-    image={require('./images/Vanshika Bansal.png')}
-      name='Vanshika Bansal'
-      POR='JOINT SECRETARY'
-      description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-      />
+          <Card
+            image={require('./images/Aviral Aggarwal.png')}
+            name='Aviral Aggarwal'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
+          <Card
+            image={require('./images/Devanshi Grover.png')}
+            name='Devanshi Grover'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
 
-    <Card 
-    
-    image={require('./images/Aayush Sharma.jpeg')}
-      name='Ayush Sharma'
-      POR='JOINT SECRETARY'
-      description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
-      />
+          />
 
 
 
-   
+          <Card
+            image={require('./images/Madhumay Bhartiya.png')}
+            name='Madhumay Bhartiya'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum'
+          />
 
-</div>
+
+
+          <Card
+            image={require('./images/Mayank Sharma.png')}
+            name='Mayank Sharma'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
+
+          <Card
+            image={require('./images/Prodorshika Ghosh.png')}
+            name='Prodorshika Ghosh'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
+          <Card
+
+            image={require('./images/Vanshika Bansal.png')}
+            name='Vanshika Bansal'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
+
+          <Card
+
+            image={require('./images/Aayush Sharma.jpeg')}
+            name='Ayush Sharma'
+            POR='JOINT SECRETARY'
+            description='Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum.'
+          />
 
 
 
-</AnimationOnScroll>
-
- 
 
 
         </div>
-    )
+
+
+
+      </AnimationOnScroll>
+
+
+
+
+    </div>
+  )
 }
