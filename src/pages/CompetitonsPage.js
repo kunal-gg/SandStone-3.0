8fb29f586915727f56518919b4a52e0e6b0c60d1
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-09-19 01:33:24 +0530

    finished the sandstone verticals page

--- src/pages/CompetitonsPage.js
+++ src/pages/CompetitonsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AnotherCompetitions from "../components/AnotherCompetitionsPage";
 import Competitions from "../components/Competitions";
 import Header from "../components/Header";
 import NewHeader from "../components/NewHeader";
@@ -6,7 +7,10 @@ export default function CompetitionsPage(){
     return(
         <>
         <NewHeader/>
+        <h1 className="fuck-off-sme2">Sandstone <span>Verticals</span></h1>
         <Competitions />
+        <AnotherCompetitions />
+        
         
         </>
     )
