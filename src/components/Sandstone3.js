5bf7bdd7882a05e7d95ce980de8d19d42f6dc30c
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-09-19 07:41:21 +0530

    changed the speakers page

--- src/components/Sandstone3.js
+++ src/components/Sandstone3.js
@@ -1,18 +1,43 @@
 export const sandstone =[
+    {name: 'Abhinav Johri', designation: 'Principal', company: 'EY', image: require('./images/Abhinav Johri.jpeg')},
+    {name: 'Ankoor Banerjee', designation: 'Cognizant', company: 'Manager GenC Hiring' , image: require('./images/Ankoor Banerjee.jpeg')},
     {name: 'Bala Thiagarajan', designation: 'Vice President', company: 'Capgemini', image: require('./images/Bala Thiagarajan.png')},
     {name: 'Gaurav Khatri', designation: 'Founder', company: 'Noise', image: require('./images/Gaurav Khatri.png')},
     {name: 'Harsh Kishore', designation: 'Director', company: 'LeadSquared', image: require('./images/Harsh Kishore.png')},,
     {name: 'Jyoti Nair', designation: 'Executive Director- Global Technology', company: 'JPMorgan Chase & Co.', image:require('./images/Jyoti Nair.png')},,
-    {name: 'Karthik SM', designation: 'Global Head - Talent Aquisition', company: 'Biocon Biologocs', image:require('./images/Karthik SM.png')},,
+    {name: 'Karthik SM', designation: 'Global Head - Talent Aquisition', company: 'Biocon Biologocs', image:require('./images/Karthik SM.png')},
+    {name: 'Kapil Sharma', designation: 'Chief Transformation Officer', company: 'Microsoft India', image: require('./images/Kapil Sharma.jpeg')}, 
+    {name: 'Manoj Bhatia', designation: 'Vice President (Asia Head Non-IT)', company: 'Morgan Stanley', },
+    {name: 'Mansi Batra' , designation: 'Herad UK Business Sales', company: 'Linkedin', image: require('./images/Mansi Batra.jpeg')},
+    {name: 'Nikhil Parwal', designation: 'Business Head', company: 'LeadSquared', image: require('./images/Nikhil Parwal.jpeg')},
+
    
     {name: 'Nishant Govil', designation: 'Managing Director -', company: 'Black Rock', image: require('./images/Nishant Govil.png')},,
     {name: 'Nitin Aggarwal', designation: 'Head Cloud AI', company: 'Google (India)', image: require('./images/Nitin Aggarwal.png')},,
+
+    {name: 'Pramil Govil', designation: 'Director', company: 'Bain & Company', image: require('./images/Pramil Govil.jpeg')},
     {name: 'Preeti Jain', designation: 'Vice President - HR', company: 'Airtel', image: require('./images/Preeti Jain.png')},,
+    
+    {name: 'Ruchira Shukla', designation: 'Head for South Asia, Disruptive Technologies', company: 'International Finance Corporation - World Bank Group', image: require('./images/Ruchira Shukla.jpeg')},
     {name: 'Sachin Gulati', designation: 'India Head - Campus Recruitment', company: 'American Express', image: require('./images/Dr. Sachin Gulati.png')},,
     {name: 'Saikat Chakraborty', designation: 'MD, Analytics Data Science', company: 'Accenture', image: require('./images/Saikat Chakraborty.png')},,
-    {name: 'Shashi Bhatia', designation: 'Human Resource', company: 'Royal Enfield', image: require('./images/Shashi Bhatia.png')},,
-    {name: 'Soumitra Dhankar', designation: 'Consulting Leader', company: 'Adobe', image: require('./images/Soumitra Dhankar.png')},,
+    
+    {name: 'Satish Rajarathnam', designation: 'SVP HR', company: 'Mphasis', image: require('./images/Satish Rajarathnam.png')},
+    {name: 'Shashi Bhatia', designation: 'Human Resource', company: 'Royal Enfield', image: require('./images/Shashi Bhatia.png')},
+    {name: 'Sheetal Pahuja', designation: 'Vice President - MIS analytics', company: 'American Express', image: require('./images/Sheetal Pahuja.jpeg')},
+       
+    
+    {name: 'Smriti Singh', designation: 'Business Lead', company: 'JP Morgan Chase and Co.', image: require('./images/Smriti Singh.jpeg')},
+    {name: 'Soumitra Dhankar', designation: 'Consulting Leader', company: 'Adobe', image: require('./images/Soumitra Dhankar.png')},
+    
+    {name: 'Suhas Joshi', designation: 'Head - Sustainability & Business stewardship South Asia', company: 'Bayer', image: require('./images/Suhas Joshi.jpeg')},,
     {name: 'Sushant Kumar', designation: 'Campus Recruitment (India)', company: 'Tata Technologies', image: require('./images/Sushant Kumar.png')},,
     {name: 'Vishal Bansal', designation: 'Talent Aquisition Head (India)', company: 'Lenovo', image: require('./images/Vishal Bansal.png')},,
     {name: 'Yoginder Grewal', designation: 'Chief Technology Office', company: 'Coca-Cola Beverages', image: require('./images/Yoginder Grewal.png')},,
+    ,
+   
+
+   
+   
+
 ]
